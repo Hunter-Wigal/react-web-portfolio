e60a29db618a671694da3ad6975019faf1f05cc8
Hunter-Wigal <64178295+Hunter-Wigal@users.noreply.github.com>
2024-01-31 22:17:28 -0500

    Add databse to skills

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -24,8 +24,8 @@ export default function About(props: {}) {
                 <p className={styles.info}>Hi, I'm Hunter. I'm currently a student at WVU, graduating in May of 2024 with a Bachelor of Science in Computer Science. After graduating, I plan to pursue
                     work in the field of software engineering / development. I'm very passionate about the field of Computer Science
                     and would eventually like to have experience in most aspects. I currently have worked with web development, graphic design
-                    with OpenGL, operating systems, and GUI's in python. An honorable mention includes work on a PyQt GUI that communicates
-                    with a rover via ROS.
+                    with OpenGL, operating systems, and GUI's in python. Some honorable mentions include work on a PyQt GUI that communicates
+                    with a rover via ROS and implementation of a database in Java utilizing hashes, I/O, and SQL style commands.
 
                     {/* Over the past couple years I've worked on multiple web applications, most of which are displayed in the projects
                     section. From those applications I've learned how to use different frameworks such as Angular, Bootstrap, and some React.
