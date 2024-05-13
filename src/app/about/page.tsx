6455d30d036e25a0f4620bc4a771dae052eb0eb1
Hunter-Wigal <hgw00002@mix.wvu.edu>
2024-05-13 12:24:34 -0400

    Update information after graduating

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -18,19 +18,25 @@ export default function About(props: {}) {
                         <h3>Phone:</h3> 304-210-3566
                     </div>
                 </div>
-                
+
             </div>
             <div className={styles.row}>
-                <p className={styles.info}>Hi, I'm Hunter. I'm currently a student at WVU, graduating in May of 2024 with a Bachelor of Science in Computer Science. After graduating, I plan to pursue
-                    work in the field of software engineering / development. I'm very passionate about the field of Computer Science
+                <p className={styles.info}>Hi, I'm Hunter. I'm a recent graduate of WVU, as of May 2024 with a Bachelor of Science in Computer Science.
+                    I'm pursuing work in the field of software engineering / development. I'm very passionate about the field of Computer Science
                     and would eventually like to have experience in most aspects. I currently have worked with web development, graphic design
                     with OpenGL, operating systems, and GUI's in python. Some honorable mentions include work on a PyQt GUI that communicates
                     with a rover via ROS and implementation of a database in Java utilizing hashes, I/O, and SQL style commands.
+                    <br/>
+                    <br/>
+                    Outside of my profession I like to pursue my various hobbies such as 3-D printing, running, and learning new topics. I enjoy 3-D printing 
+                    new things, either for amusement or to solve problems around the house. I run during the spring and summer, usually
+                    to train for half-marathons. I also like to learn new programming languages / technology for topics such as game development. 
 
                     {/* Over the past couple years I've worked on multiple web applications, most of which are displayed in the projects
                     section. From those applications I've learned how to use different frameworks such as Angular, Bootstrap, and some React.
                     I've also learned how to write in Javscript/Typescript and CSS while working on these projects. */}
                 </p>
+
 
             </div>
         </div>
