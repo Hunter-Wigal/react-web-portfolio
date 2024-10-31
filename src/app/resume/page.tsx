62c4f1dd482103589b678110c44d9248de360b77
Hunter-Wigal <hgw00002@mix.wvu.edu>
2024-10-30 20:03:56 -0400

    Change portfolio links to s3 bucket and fix issue with other browsers

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -8,11 +8,11 @@ export default function ResumePage(props: {}) {
     return (<div className="body">
         <h1 className="title">Resume</h1>
 
-        <img className={styles.resume} src={"/resume.png"}
+        <img className={styles.resume} src={"https://portfolio-static-files.s3.us-east-2.amazonaws.com/Hunter-Wigal+Resume+Software.png"}
             alt="This is an image of my resume" />
         <div className={styles.download}>
             <p>You can download my resume here:</p>
-            <a id="a" href="/Hunter-Wigal-Resume.pdf" download>Hunter-Wigal-Resume.pdf</a>
+            <a id="a" href="https://portfolio-static-files.s3.us-east-2.amazonaws.com/Hunter-Wigal+Resume+Software.pdf" download>Hunter-Wigal-Resume.pdf</a>
         </div>
     </div>);
 }
